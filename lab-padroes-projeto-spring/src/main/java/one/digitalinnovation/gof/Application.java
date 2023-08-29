2ab2b9557eabd7a8f71ad018dc5e6208d4504408
Wallace <wallaceste@hotmail.com>
2023-08-28 22:08:40 -0300

    Final Lab Padrões Projetos DIO

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,29 @@
+/*
+ * Projeto Spring Boot gerado via Spring Initializr.
+ * Os seguintes módulos foram selecionados:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ *
+ * @author wallace_stevan
+ *
+ */
+
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
